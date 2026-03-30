7fccc97edfc57fb4f752b3f53568522a67f290bc
Jorge <jorge.vieira0713@gmail.com>
2026-03-30 12:48:01 -0300

    Refactor movie recommendation logic and UI

--- app.py
+++ app.py
@@ -170,9 +170,9 @@ for movie_id_rec in rec_ids:
     row = df_peliculas[df_peliculas['movie_id'] == movie_id_rec].iloc[0]
     titulo = row['titulo']
 
-        if genero_select != "Todos":
-            if row[genero_select] != 1:
-                 continue
+    if genero_select != "Todos":
+        if row[genero_select] != 1:
+            continue
 
             # Llamamos a la función actualizada
             poster, rating = get_movie_details(titulo)
